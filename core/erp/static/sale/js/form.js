438e3bf4e8d7cf09235411925de3aab775311633
nestormarlier <47480842+nestormarlier@users.noreply.github.com>
2021-03-27 16:08:36 -0300

    Primer commit desde aplicacion estable hasta el datetimepicker

---
+++ core/erp/static/sale/js/form.js
@@ -0,0 +1,13 @@
+$(function () {
+    $('.select2').select2({
+        theme: "bootstrap4",
+        language: 'es'
+    });
+
+    $('#date_joined').datetimepicker({
+        format: 'YYYY-MM-DD',
+        date: moment().format("YYYY-MM-DD"),
+        locale: 'es',
+        //minDate: moment().format("YYYY-MM-DD")
+    });
+});
